118a4bb05c02b78552185a67a5fa439fd41c4805
John Edmerson Pizarra <edmerpizarra@gmail.com>
2023-08-07 12:25:03 +0800

    feat: implement user check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,37 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import useApi, { ITerritory } from "../hooks/useApi";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return <></>;
+  const [territories, setTerritories] = useState([] as ITerritory[] | null);
+  const { userData, error, loading, getTerritories } = useApi();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (userData) {
+      getTerritories().then((data) => {
+        setTerritories(data);
+      });
+    } else {
+      router.push("/account/login");
+    }
+  }, [userData]);
+
+  return (
+    <>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+
+      <h1>Territories</h1>
+      {/* {territories && (
+        <ul>
+          {territories.map((territory) => (
+            <li key={territory.id}>{territory.name}</li>
+          ))}
+        </ul>
+      )} */}
+    </>
+  );
 }
